5e45c6d9acdaeb46c4d435c3f37e69291a9bc4f5
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2020-05-09 22:09:53 +0100

    Updating config information

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MehdiNV`,
+    description: `Personal website of Mehdi Naderi Varandi`,
+    author: `Mehdi Naderi Varandi, made using @gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- website/src/pages/404.js
+++ website/src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Something went wrong!</h1>
+    <p>Unfortunately something, somewhere, went wrong. This route doesn't exist</p>
   </Layout>
 )
 
