1a2d9950389e1cb8c3cd359bd45ff012dd754c10
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2020-05-16 23:59:04 +0100

    Changed background text, made it more ux-wiser

--- website/src/components/background.js
+++ website/src/components/background.js
@@ -9,12 +9,16 @@ const Background = () => {
       <div class="backgroundSection">
 
         <div id="backgroundContent">
-          <h3> Hi there! </h3>
+          <h3> Hi there! 👋</h3>
           <p>I'm Mehdi, a Computer Science undergraduate at Newcastle University. At the moment, I'm
-            working as a Software Engineering Intern at WonderBill!
+            working as a Software Engineering Intern at WonderBill! </p>
 
-            Outside of programming, I can usually be found fencing, teaching or debating.
-          </p>
+          <p>I'm passionate about Technology in all its aspects, whether it be designing innovative
+            solutions or managing teams of Computer Science mentors in order to teach CS in the local community and inspire the next generation of coders.
+            Outside of programming, I can usually be found fencing,
+            teaching or debating</p>
+
+          <p id="accolades">Accolades:🏆</p>
         </div>
 
         <div id="backgroundImage">
