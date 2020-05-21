11f847c2021f519ce2c31bb2b0c7c2466a277bc6
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2020-05-21 03:03:17 +0100

    Renamed file, importing styled.components in, set up boilerplate

---
+++ website/src/components/backgroundImagesDivs.js
@@ -0,0 +1,18 @@
+import rightSideImage from '../images/firstpatternsvg.svg';
+import leftSideImage from '../images/secondpatternsvg.svg';
+import footerImage from '../images/thirdpatternsvg.svg';
+
+import styled from 'styled-components';
+
+export const RightBackgroundPattern = styled.div`
+
+
+`;
+
+export const LeftBackgroundPattern = styled.div`
+
+`;
+
+export const footerBackgroundPattern = styled.div`
+
+`
