27d17465da35dfe4b3dcad8991ab2e712af4d1bf
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2025-10-17 15:32:27 +1100

    Delete photography sub-page due to over-complexity and headache

--- javascript/sanityClient.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from 'https://cdn.skypack.dev/@sanity/client';
-
-export const client = sanityClient({
-  projectId: 'e63aw6jk',
-  dataset: 'production',
-  useCdn: true,
-  apiVersion: '2025-01-01'
-});
--- studio/schemaTypes/index.js
+++
@@ -1,4 +0,0 @@
-import photo from './photo';
-import shuffleTracker from './shuffleTracker';
-
-export const schemaTypes = [photo, shuffleTracker];
